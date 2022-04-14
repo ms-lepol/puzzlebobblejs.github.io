43d34e6b65444eeb82e06f081d67c6f9cc394a77
Tristan <102792327+TriTrisTouiL@users.noreply.github.com>
2022-04-14 17:48:37 +0200

    Update verifBulles.js

--- verifBulles.js
+++ verifBulles.js
@@ -3,26 +3,18 @@ function verifBulles(matBulles, lance){
     Entrees : matrice de Bulles sur laquelle on joue et la Bulle lancee par le canon
     Sortie : nbSame l'entier du nbr de voisins identiques*/
     
-    let tabSameVoisins = []; // le tableau des Bulles voisines qu'on va retourner
-    
-    tabVoisins = voisins(matBulles, lance);
-    
-    for (i = 0; i<tabVoisins.length; i++){
-        if (lance.color = matBulles[tabVoisins[i].y][tabVoisins[i].x]){
-            tabSameVoisins.push(new Bulle(tabVoisins[i].x,tabVoisins[i].y,tabVoisins[i].color));
-        }
-    } 
-    
+    tabSameVoisins = sameVoisins(matBulles, lance);
+
     /*On retourne maintenant le tableau de voisin identiques si sa longueur est differente de 1
     car il n'y a pas d'indetermination puisque a 0 il ne se passe rien alors qu'au
     dessus de 2 on fait forcement tomber.*/
     if (tabSameVoisins.length == 0 ){
-        return tabSameVoisins;
+        return 0;
     }
     else if (tabSameVoisins == 1 && 
-        verifBulles(matBulles, tabSameVoisins[0]).length == 1) {
+            sameVoisins(matBulles, tabSameVoisins[0]).length == 1) {
 
-        return tabSameVoisins;
+        return 0;
     }
     /*Dans le cas ou la longueur de tabVoisins = 1, cela signifie que la Bulle lancee a autour 
     d'elle un seul voisin de meme couleur. Or ce voisin peut avoir lui un 
@@ -30,15 +22,35 @@ function verifBulles(matBulles, lance){
     Donc on ajoute au nbSame le nbSame du voisin moins 1 car on ne 
     compte pas la Bulle deja lancee.*/
     else {
-        for (i = 0; i < tabSameVoisins.length; i++){
-            nvTabSameVoisins = verifBulles(matBulles, tabSameVoisins[i]);
-                for (j = 0; j < nvTabSameVoisins.length; j++){
-                    tabSameVoisins.push(nvTabSameVoisins[j]);
-                    tabSameVoisins.splice(tabVoisins.indexOff(lance),1);
-                }
-        }
-        return tabSameVoisins;
+        matBulles[lance.pos.y][lance.pos.x] = '0';
+        breakV(lance);
     }
+}
+
+function breakV(matBulles, bulle){
+    tabV = sameVoisins(matBulles, bulle);
+    if (tabV.length == 0){
+        return 0;
+    }
+    else {
+        for (i = 0; i < tabV.length; i++ ){
+            return breakV(matBulles, tabV[i]);
+            matBulles[tabV[i].y][tabV[i].x] = '0';
+        }
+    }
+}
+
+function sameVoisins(matBulles, lance){
+    let tabSameVoisins = []; // le tableau des Bulles voisines qu'on va retourner
+    
+    tabVoisins = voisins(matBulles, lance);
+    
+    for (i = 0; i<tabVoisins.length; i++){
+        if (lance.color == matBulles[tabVoisins[i].y][tabVoisins[i].x]){
+            tabSameVoisins.push(new Bulle(tabVoisins[i].x,tabVoisins[i].y,tabVoisins[i].color));
+        }
+    }
+    return tabSameVoisins;
 }
 
 function voisins(matBulles, lance){
